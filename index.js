8b172491d7a9f3e5f9832eba67f293509bee846a
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-07 19:14:32 +0300

    commit tree fbdf4797b28b285ffa95080f889cfccbff99c3f4

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//multiplication
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+//randomInt
+let random = Math.floor(Math.random()) + 1;
+console.log(random);
+
+//mod
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+//max
+let max = Math.max(8,3,19,20);
+console.log(max);
